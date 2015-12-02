62cce1c520360e5de17fd403013b92a8723ad0fb
Study <1439504683@qq.com>
2015-12-02 17:17:06 +0800

    初始化提交(Initialization Submit)

---
+++ zhbj/src/main/java/com/tofirst/study/zhbj/SpalshActivity.java
@@ -0,0 +1,16 @@
+package com.tofirst.study.zhbj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+
+
+
+    }
+}
